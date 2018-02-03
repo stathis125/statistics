a7c53f527784aa37b5cd879a89d123d111ba088b
Stathis Tsan <stathistsan@hotmail.com>
2018-02-03 18:36:43 +0200

    Moving everything in 1 server

--- routeHandlers/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint global-require: "off" */
-module.exports = {
-    getAllEmployeesHandler: require('./getAllEmployeesHandler'),
-    getEmployeeById: require('./getEmployeeById'),
-    putUpdateEmployee: require('./putUpdateEmployee'),
-    deleteEmployeeById: require('./deleteEmployeeById'),
-    postCreateEmployee: require('./postCreateEmployee')
-};
